aa38f562149044b4650681f564b9bdfbbb017bd1
Adrian Puscuta <adrian.puscu@gmail.com>
2023-11-15 22:30:55 +0200

    Implemented all EmailService methods.

--- src/main/java/application/com/orangeteam/services/EmailServiceImpl.java
+++ src/main/java/application/com/orangeteam/services/EmailServiceImpl.java
@@ -2,6 +2,8 @@ package application.com.orangeteam.services;
 
 import application.com.orangeteam.models.dtos.BookingDTO;
 import application.com.orangeteam.models.dtos.CustomerDTO;
+import application.com.orangeteam.models.dtos.PaymentDTO;
+import application.com.orangeteam.models.dtos.TravelPackageDTO;
 import com.mailslurp.apis.InboxControllerApi;
 import com.mailslurp.clients.ApiClient;
 import com.mailslurp.clients.ApiException;
@@ -14,6 +16,8 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -61,23 +65,47 @@ public class EmailServiceImpl implements EmailService {
     public void sendBookingConfirmationEmail(CustomerDTO customerDTO, BookingDTO bookingDTO) {
         Context context = new Context();
         context.setVariable("firstName", customerDTO.getFirstName());
-//        context.setVariable("bookingId", BookingDTO.getId);
+        context.setVariable("bookingId", bookingDTO.getId());
         String emailContent = emailTemplateEngine.process("booking-confirmation", context);
         sendEmail("Your booking is confirmed", emailContent, customerDTO.getEmail());
     }
 
     @Override
-    public void sendPaymentReceiptEmail(CustomerDTO customerDTO) {
-
+    public void sendPaymentReceiptEmail(CustomerDTO customerDTO, PaymentDTO paymentDTO) {
+        Context context = new Context();
+        context.setVariable("firstName", customerDTO.getFirstName());
+        context.setVariable("bookingID", paymentDTO.getBookingID());
+        context.setVariable("paymentID", paymentDTO.getId());
+        String emailContent = emailTemplateEngine.process("payment-successful", context);
+        sendEmail("Payment failed", emailContent, customerDTO.getEmail());
     }
 
     @Override
-    public void sendPaymentFailedEmail(CustomerDTO customerDTO) {
-
+    public void sendPaymentFailedEmail(CustomerDTO customerDTO, PaymentDTO paymentDTO) {
+        Context context = new Context();
+        context.setVariable("firstName", customerDTO.getFirstName());
+        context.setVariable("bookingID", paymentDTO.getBookingID());
+        context.setVariable("paymentID", paymentDTO.getId());
+        String emailContent = emailTemplateEngine.process("payment-failed", context);
+        sendEmail("Payment failed", emailContent, customerDTO.getEmail());
     }
 
     @Override
-    public void sendItineraryChangeEmail(CustomerDTO customerDTO, BookingDTO bookingDTO) {
+    public void sendItineraryChangeEmail(CustomerDTO customerDTO, BookingDTO bookingDTO,
+                                         TravelPackageDTO oldTravelPackageDTO, TravelPackageDTO updatedTravelPackage) {
+        Map<String, Object> contextMap = new HashMap<>();
+        contextMap.put("oldStartDate", oldTravelPackageDTO.getStartingDate());
+        contextMap.put("oldEndDate", oldTravelPackageDTO.getEndingDate());
+        contextMap.put("oldDestination", oldTravelPackageDTO.getDestination());
+        contextMap.put("updatedStartDate", updatedTravelPackage.getStartingDate());
+        contextMap.put("updatedEndDate", updatedTravelPackage.getEndingDate());
+        contextMap.put("updatedDestination", updatedTravelPackage.getDestination());
+        contextMap.put("firstName", customerDTO.getFirstName());
+        contextMap.put("bookingID", bookingDTO.getId());
 
+        Context context = new Context();
+        context.setVariables(contextMap);
+        String emailContent = emailTemplateEngine.process("itinerary-change", context);
+        sendEmail("Itinerary change for your booking", emailContent, customerDTO.getEmail());
     }
 }
